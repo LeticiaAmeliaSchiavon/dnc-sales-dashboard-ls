22235365dbbad90ba9e27454e308cba83944e2a7
Letícia Schiavon <leticiaschiavon20@gmail.com>
2025-02-06 16:38:36 -0400

    Feat material-ui-insert

---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const BannerImage = styled.div`
+  background-image: url(/login-image.svg);
+  backgroun-size: cover;
+  height: 100vh;
+  width: 50vw;
+`
