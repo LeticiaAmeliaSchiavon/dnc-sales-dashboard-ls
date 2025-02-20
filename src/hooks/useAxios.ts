9e7369b79ac9e3e8fa2f7e6b88552e427bdf2381
Letícia Schiavon <leticiaschiavon20@gmail.com>
2025-02-19 20:23:28 -0400

    Feat: Profile Request

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -1,56 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import Cookies from 'js-cookie'
 import { useState, useEffect } from 'react'
 import axios, { AxiosRequestConfig } from 'axios'
-import Cookies from 'js-cookie'
 
-const axioInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/`,
 })
 
-export const usePost = <T, P>(endpoint: string) => {
-  const [data, setData] = useState<T | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<number | null>(null)
-
-  const postData = async (postData: P, config?: AxiosRequestConfig) => {
-    setData(null)
-    setLoading(true)
-    setError(null)
-
-    try {
-      const response = await axioInstance({
-        url: endpoint,
-        method: 'POST',
-        data: postData,
-        headers: {
-          'Content-Type': 'application/json',
-          ...config?.headers,
-        },
-        ...config,
-      })
-      setData(response.data)
-    } catch (e: any) {
-      setError(e.response.status ?? 500)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  return { data, loading, error, postData }
-}
-
 export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
   const [data, setData] = useState<T | null>(null)
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<number | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
 
   const getData = async () => {
     setLoading(true)
     setError(null)
 
     try {
-      const response = await axioInstance({
+      const response = await axiosInstance({
         url: endpoint,
         method: 'GET',
         headers: {
@@ -61,7 +29,7 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
       })
       setData(response.data)
     } catch (e: any) {
-      setError(e.response.status ?? 500)
+      setError(e.response?.status ?? 500)
     } finally {
       setLoading(false)
     }
@@ -72,4 +40,103 @@ export const useGet = <T>(endpoint: string, config?: AxiosRequestConfig) => {
   }, [])
 
   return { data, loading, error, getData }
+}
+
+export const usePost = <T, P>(endpoint: string, withAuth?: boolean) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
+
+  const postData = async (postData: P, config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+    setError(null)
+
+    try {
+      const headers = withAuth
+        ? {
+            Authorization: `Bearer ${Cookies.get('Authorization')}`,
+            'Content-Type': 'application/json',
+            ...config?.headers,
+          }
+        : config?.headers
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'POST',
+        data: postData,
+        headers,
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      setError(e.response?.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, error, postData }
+}
+
+export const usePut = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<number | null>(null)
+
+  const putData = async (putData: T, config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+    setError(null)
+
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
+          'Content-Type': 'application/json',
+          ...config?.headers,
+        },
+        data: putData,
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      setError(e.response?.status ?? 500)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, error, putData }
+}
+
+export const useDelete = <T>(endpoint: string) => {
+  const [data, setData] = useState<T | null>(null)
+  const [loading, setLoading] = useState(false)
+
+  const deleteData = async (config?: AxiosRequestConfig) => {
+    setData(null)
+    setLoading(true)
+
+    try {
+      const response = await axiosInstance({
+        url: endpoint,
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${Cookies.get('Authorization')}`,
+          'Content-Type': 'application/json',
+          ...config?.headers,
+        },
+        ...config,
+      })
+      setData(response.data)
+    } catch (e: any) {
+      throw e.response?.status
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { data, loading, deleteData }
 }
