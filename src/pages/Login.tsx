69e7fab27a988cb61da43ff97621bb7583b77331
Letícia Schiavon <leticiaschiavon20@gmail.com>
2025-02-21 14:56:04 -0400

    Feat: Create Profile Flow

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -26,8 +26,15 @@ import { jwtExpirationDateConverter, pxToRem } from '@/utils'
 // TYPES
 import { DecodedJWT, MessageProps, LoginData, LoginPostData } from '@/types'
 
+// REDUX
+import { useSelector } from 'react-redux'
+import { RootState } from '@/redux'
+
 function Login() {
   const navigate = useNavigate()
+  const { email, message } = useSelector(
+    (state: RootState) => state.createProfile
+  )
   const inputs = [
     { type: 'email', placeholder: 'Email' },
     { type: 'password', placeholder: 'Senha' },
@@ -38,7 +45,7 @@ function Login() {
   const { formValues, formValid, handleChange } = useFormValidation(inputs)
 
   const handleMessage = (): MessageProps => {
-    if (!error) return { msg: '', type: 'success' }
+    if (!error) return { msg: message ?? '', type: 'success' }
     switch (error) {
       case 401:
         return {
@@ -71,6 +78,12 @@ function Login() {
     }
     if (Cookies.get('Authorization')) navigate('/home')
   }, [data, navigate])
+
+  useEffect(() => {
+    if (email) {
+      handleChange(0, email)
+    }
+  }, [email])
 
   return (
     <>
